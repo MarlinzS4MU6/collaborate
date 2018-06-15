92013c170464b46130632b5b1518be06ba4d8411
John Bordano <john@Johns-MacBook.attlocal.net>
2018-06-15 15:43:25 -0500

    Define a default_scope for posts

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,11 +1,19 @@
 module PostsHelper
-  def post_format_partial_path
-    current_page?(root_path) ? 'posts/post/home_page' : 'posts/post/branch_page'
-  end
-  
-  def no_posts_partial_path
-    @posts.empty? ? 'posts/branch/no_posts' : 'shared/empty_partial'
-  end
+    def category_field_partial_path
+      if params[:category].present?
+        'posts/branch/search_form/category_field'
+      else
+        'shared/empty_partial'
+      end
+    end
+
+    def post_format_partial_path
+      current_page?(root_path) ? 'posts/post/home_page' : 'posts/post/branch_page'
+    end
+
+    def no_posts_partial_path
+      @posts.empty? ? 'posts/branch/no_posts' : 'shared/empty_partial'
+    end
 
     def create_new_post_partial_path
       if user_signed_in?
